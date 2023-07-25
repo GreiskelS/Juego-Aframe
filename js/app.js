1b9059c85875c209ee420846dae2dc3c879f3ab9
Greiskel <greiskel.sanchez@gmail.com>
2023-07-25 12:37:10 +0200

    modificando sombras en escena

--- js/app.js
+++ js/app.js
@@ -4,14 +4,14 @@
 window.addEventListener('load', initScene)
 
 const meteors = [
-    { x: 0, y: 0, z: -30 },
-    { x: 0, y: 0, z: 30 },
-    { x: 30, y: 0, z: 0 },
-    { x: -30, y: 0, z: 0 },
-    { x: 20, y: 0, z: 20 },
-    { x: 20, y: 0, z: -20 },
-    { x: -20, y: 0, z: -20 },
-    { x: -20, y: 0, z: 20 }
+    { x: 0, y: 0, z: -60 },
+    { x: 0, y: 0, z: 60 },
+    { x: 20, y: 0, z: 0 },
+    { x: -20, y: 0, z: 0 },
+    { x: 10, y: 0, z: 40 },
+    { x: 10, y: 0, z: -40 },
+    { x: -10, y: 0, z: -40 },
+    { x: -10, y: 0, z: 40 }
 ]
 
 let meteor, score = 0
@@ -24,7 +24,7 @@ function initScene() {
 
         meteors.forEach(pos => {
 
-            meteor = document.createElement('a-entity')
+            meteor = document.createElement('a-entity light="type: directional; color: #FFF; intensity: 0.6"')
             meteor.setAttribute('geometry', { primitive: 'sphere', radius: Math.random() * 3 + 0.5 })
             meteor.setAttribute('material', { shader: 'flat', src: '#meteor' })
             meteor.setAttribute('class', 'meteor')
@@ -54,13 +54,14 @@ function initScene() {
   
       meteors.forEach(pos => {
   
-        meteor = document.createElement('a-entity')
-        meteor.setAttribute('gltf-model', 'https://cdn.glitch.global/ebd0de65-7a0c-4c3d-a550-d214da511d08/Turret%20Teleporter.glb?v=1690187026653') // Aquí usamos el glTF previamente cargado
-        meteor.setAttribute('material', { color: '#00FF00' }) // Puedes configurar el material como desees
+        meteor = document.createElement('a-entity' )
+        meteor.setAttribute('gltf-model', 'https://cdn.glitch.global/ebd0de65-7a0c-4c3d-a550-d214da511d08/Turret%20Teleporter.glb?v=1690187026653', 
+        ) // Aquí usamos el glTF previamente cargado
+        meteor.setAttribute('material', { color: 'white' }) // Puedes configurar el material como desees
         meteor.setAttribute('class', 'meteor')
         meteor.object3D.position.set(pos.x, pos.y, pos.z)
 
-        meteor.setAttribute('scale', '4 4 4');
+        meteor.setAttribute('scale', '9 9 9 ');
   
         meteor.setAttribute('shootable', '')
   
