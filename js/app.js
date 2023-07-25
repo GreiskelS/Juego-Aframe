af22fd991e990f3eb7004aaa436a604412752bf2
Kibria <kibriare@gmail.com>
2023-07-25 10:17:11 +0200

    subiendo localhost

--- js/app.js
+++ js/app.js
@@ -1,6 +1,3 @@
-
-
-
 window.addEventListener('load', initScene)
 
 const meteors = [
@@ -40,31 +37,25 @@ function initScene() {
 AFRAME.registerComponent('shootable', {
     init: function () {
         this.el.addEventListener('click', () => {
-            this.el.parentNode.removeChild(this.el)
-            document.querySelector('[text]').setAttribute('value', `${++score} meteoritos cazados`)
-        })
+            // Increment the score
+            score++;
+            document.querySelector('[text]').setAttribute('value', `${score} meteoritos cazados`);
+
+            // Remove the meteor from the scene
+            this.el.parentNode.removeChild(this.el);
+
+            // Store the updated score in LocalStorage
+            localStorage.setItem('score', score);
+        });
+    },
+});
+
+window.addEventListener('load', () => {
+    const savedScore = localStorage.getItem('score');
+    if (savedScore !== null) {
+        score = parseInt(savedScore);
+        document.querySelector('[text]').setAttribute('value', `${score} meteoritos cazados`);
     }
-})
 
-
-function initScene() {
-    let orbits = document.querySelectorAll('.orbit')
-  
-    orbits.forEach(orbit => {
-  
-      meteors.forEach(pos => {
-  
-        meteor = document.createElement('a-entity')
-        meteor.setAttribute('gltf-model', 'https://cdn.glitch.global/ebd0de65-7a0c-4c3d-a550-d214da511d08/Turret%20Teleporter.glb?v=1690187026653') // Aquí usamos el glTF previamente cargado
-        meteor.setAttribute('material', { color: '#00FF00' }) // Puedes configurar el material como desees
-        meteor.setAttribute('class', 'meteor')
-        meteor.object3D.position.set(pos.x, pos.y, pos.z)
-
-        meteor.setAttribute('scale', '4 4 4');
-  
-        meteor.setAttribute('shootable', '')
-  
-        orbit.appendChild(meteor)
-      })
-    })
-  }
+    initScene();
+});
