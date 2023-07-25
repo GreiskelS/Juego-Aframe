629a7a6d273770a8c7a766a0a94a1e5e82b7c3be
Greiskel <greiskel.sanchez@gmail.com>
2023-07-25 18:27:58 +0200

    añadiendo sonidos

--- js/app.js
+++ js/app.js
@@ -2,38 +2,38 @@
 window.addEventListener('load', initScene)
 
 const meteors = [
-    { x: 0, y: 0, z: -30 },
-    { x: 0, y: 0, z: 30 },
-    { x: 30, y: 0, z: 0 },
-    { x: -30, y: 0, z: 0 },
-    { x: 20, y: 0, z: 20 },
-    { x: 20, y: 0, z: -20 },
-    { x: -20, y: 0, z: -20 },
-    { x: -20, y: 0, z: 20 }
+  { x: 0, y: 0, z: -60 },
+  { x: 0, y: 0, z: 60 },
+  { x: 20, y: 0, z: 0 },
+  { x: -20, y: 0, z: 0 },
+  { x: 10, y: 0, z: 40 },
+  { x: 10, y: 0, z: -40 },
+  { x: -10, y: 0, z: -40 },
+  { x: -10, y: 0, z: 40 }
 ]
 
 let meteor, score = 0
 
-function initScene() {
+// function initScene() {
 
-    let orbits = document.querySelectorAll('.orbit')
+//     let orbits = document.querySelectorAll('.orbit')
 
-    orbits.forEach(orbit => {
+//     orbits.forEach(orbit => {
 
-        meteors.forEach(pos => {
+//         meteors.forEach(pos => {
 
-            meteor = document.createElement('a-entity')
-            meteor.setAttribute('geometry', { primitive: 'sphere', radius: Math.random() * 3 + 0.5 })
-            meteor.setAttribute('material', { shader: 'flat', src: '#meteor' })
-            meteor.setAttribute('class', 'meteor')
-            meteor.object3D.position.set(pos.x, pos.y, pos.z)
+//             meteor = document.createElement('a-entity')
+//             meteor.setAttribute('geometry', { primitive: 'sphere', radius: Math.random() * 3 + 0.5 })
+//             meteor.setAttribute('material', { shader: 'flat', src: '#meteor' })
+//             meteor.setAttribute('class', 'meteor')
+//             meteor.object3D.position.set(pos.x, pos.y, pos.z)
 
-            meteor.setAttribute('shootable', '')
+//             meteor.setAttribute('shootable', '')
 
-            orbit.appendChild(meteor)
-        })
-    })
-}
+//             orbit.appendChild(meteor)
+//         })
+//     })
+// }
 
 window.addEventListener('load', initScene)
 
@@ -80,7 +80,7 @@ function initScene() {
         meteor.setAttribute('class', 'meteor')
         meteor.object3D.position.set(pos.x, pos.y, pos.z)
 
-        meteor.setAttribute('scale', '4 4 4');
+        meteor.setAttribute('scale', '9 9 9');
   
         meteor.setAttribute('shootable', '')
   
