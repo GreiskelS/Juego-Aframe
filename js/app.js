856c74fcdc7c052285043266908ec4cf9a077570
Greiskel <greiskel.sanchez@gmail.com>
2023-07-25 18:24:20 +0200

    añadiendo sonidos

--- js/app.js
+++ js/app.js
@@ -1,79 +1,70 @@
 
 window.addEventListener('load', initScene)
 
-const naves = [
-    { x: 0, y: 0, z: -60 },
-    { x: 0, y: 0, z: 60 },
-    { x: 20, y: 0, z: 0 },
-    { x: -20, y: 0, z: 0 },
-    { x: 10, y: 0, z: 40 },
-    { x: 10, y: 0, z: -40 },
-    { x: -10, y: 0, z: -40 },
-    { x: -10, y: 0, z: 40 }
+const meteors = [
+    { x: 0, y: 0, z: -30 },
+    { x: 0, y: 0, z: 30 },
+    { x: 30, y: 0, z: 0 },
+    { x: -30, y: 0, z: 0 },
+    { x: 20, y: 0, z: 20 },
+    { x: 20, y: 0, z: -20 },
+    { x: -20, y: 0, z: -20 },
+    { x: -20, y: 0, z: 20 }
 ]
-
-
-// function initScene() {
-
-//     let orbits = document.querySelectorAll('.orbit')
-
-//     orbits.forEach(orbit => {
-
-//         naves.forEach(pos => {
-
-//             navee = document.createElement('a-entity light="type: directional; color: #FFF; intensity: 0.6" sound: "on click"')
-//             // navee.setAttribute('geometry', { primitive: 'sphere', radius: Math.random() * 3 + 0.5 })
-//             navee.setAttribute('gltf-model', 'https://cdn.glitch.global/ebd0de65-7a0c-4c3d-a550-d214da511d08/Turret%20Teleporter.glb?v=1690187026653'), 
-
-//             // navee.setAttribute('material', { shader: 'flat', src: '#navee' })
-//             navee.setAttribute('class', 'navee')
-//             navee.object3D.position.set(pos.x, pos.y, pos.z)
-
-//             navee.setAttribute('material', { color: 'white', Roboto: 'monospace', src: '#navee' }) // Puedes configurar el material como desees
-
-//             navee.setAttribute('shootable', '')
-
-//             orbit.appendChild(navee)
-//         })
-//     })
-// }
-
 
 let meteor, score = 0
 
-// function initScene() {
+function initScene() {
 
-//     let orbits = document.querySelectorAll('.orbit')
+    let orbits = document.querySelectorAll('.orbit')
 
-//     orbits.forEach(orbit => {
+    orbits.forEach(orbit => {
 
-//         meteors.forEach(pos => {
+        meteors.forEach(pos => {
 
-//             meteor = document.createElement('a-entity')
-//             meteor.setAttribute('geometry', { primitive: 'sphere', radius: Math.random() * 3 + 0.5 })
-//             meteor.setAttribute('material', { shader: 'flat', src: '#meteor' })
-//             meteor.setAttribute('class', 'meteor')
-//             meteor.object3D.position.set(pos.x, pos.y, pos.z)
+            meteor = document.createElement('a-entity')
+            meteor.setAttribute('geometry', { primitive: 'sphere', radius: Math.random() * 3 + 0.5 })
+            meteor.setAttribute('material', { shader: 'flat', src: '#meteor' })
+            meteor.setAttribute('class', 'meteor')
+            meteor.object3D.position.set(pos.x, pos.y, pos.z)
 
-//             navee.setAttribute('shootable', '')
+            meteor.setAttribute('shootable', '')
 
-//             orbit.appendChild(navee)
-//         })
-//     })
-// }
+            orbit.appendChild(meteor)
+        })
+    })
+}
+
+window.addEventListener('load', initScene)
 
 //funcion de disparar nave
 AFRAME.registerComponent('shootable', {
     init: function () {
         this.el.addEventListener('click', () => {
-            this.el.parentNode.removeChild(this.el)
-            document.querySelector('[text]').setAttribute('value', `${++score} meteoritos cazados`)
-        })
-    },
+            // Increment the score
+            score++;
+            document.querySelector('[text]').setAttribute('value', `${score} meteoritos cazados`);
 
-    initScene(){
-},
-})
+            // Remove the meteor from the scene
+            this.el.parentNode.removeChild(this.el);
+
+            // Store the updated score in LocalStorage
+            localStorage.setItem('score', score);
+        });
+    },
+});
+
+// guardarlo en el local storage
+window.addEventListener('load', () => {
+    const savedScore = localStorage.getItem('score');
+    if (savedScore !== null) {
+        score = parseInt(savedScore);
+        document.querySelector('[text]').setAttribute('value', `${score} meteoritos cazados`);
+    }
+
+    initScene();
+});
+
 
 // meter el objeto del nave
 function initScene() {
@@ -81,16 +72,16 @@ function initScene() {
   
     orbits.forEach(orbit => {
   
-      naves.forEach(pos => {
+      meteors.forEach(pos => {
   
-        meteor = document.createElement('a-entity' )
-        meteor.setAttribute('gltf-model', 'https://cdn.glitch.global/ebd0de65-7a0c-4c3d-a550-d214da511d08/Turret%20Teleporter.glb?v=1690187026653'), 
-         // Aquí usamos el glTF previamente cargado
-        meteor.setAttribute('material', { color: 'white', Roboto: 'monospace'}) // Puedes configurar el material como desees
+        meteor = document.createElement('a-entity')
+        meteor.setAttribute('gltf-model', 'https://cdn.glitch.global/ebd0de65-7a0c-4c3d-a550-d214da511d08/Turret%20Teleporter.glb?v=1690187026653') // Aquí usamos el glTF previamente cargado
+        meteor.setAttribute('material', { color: '#00FF00' }) // Puedes configurar el material como desees
         meteor.setAttribute('class', 'meteor')
         meteor.object3D.position.set(pos.x, pos.y, pos.z)
 
-        meteor.setAttribute('scale', '9 9 9 ' );
+        meteor.setAttribute('scale', '4 4 4');
+  
         meteor.setAttribute('shootable', '')
   
         orbit.appendChild(meteor)
