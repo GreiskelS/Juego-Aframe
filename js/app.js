bf1e3951c28005de44b1313e1e08e820fcbd4e2e
Kibria <kibriare@gmail.com>
2023-07-26 09:55:00 +0200

    com

--- js/app.js
+++ js/app.js
@@ -1,6 +1,3 @@
-
-
-
 window.addEventListener('load', initScene)
 
 const meteors = [
@@ -37,43 +34,7 @@ function initScene() {
     })
 }
 
-window.addEventListener('load', initScene)
-
-const meteors = [
-    { x: 0, y: 0, z: -30 },
-    { x: 0, y: 0, z: 30 },
-    { x: 30, y: 0, z: 0 },
-    { x: -30, y: 0, z: 0 },
-    { x: 20, y: 0, z: 20 },
-    { x: 20, y: 0, z: -20 },
-    { x: -20, y: 0, z: -20 },
-    { x: -20, y: 0, z: 20 }
-]
-
-let meteor, score = 0
-
-function initScene() {
-
-    let orbits = document.querySelectorAll('.orbit')
-
-    orbits.forEach(orbit => {
-
-        meteors.forEach(pos => {
-
-            meteor = document.createElement('a-entity')
-            meteor.setAttribute('geometry', { primitive: 'sphere', radius: Math.random() * 3 + 0.5 })
-            meteor.setAttribute('material', { shader: 'flat', src: '#meteor' })
-            meteor.setAttribute('class', 'meteor')
-            meteor.object3D.position.set(pos.x, pos.y, pos.z)
-
-            meteor.setAttribute('shootable', '')
-
-            orbit.appendChild(meteor)
-        })
-    })
-}
-
-//funcion de disparar nave
+//funcion de disparar
 AFRAME.registerComponent('shootable', {
     init: function () {
         this.el.addEventListener('click', () => {
@@ -90,7 +51,7 @@ AFRAME.registerComponent('shootable', {
     },
 });
 
-// guardarlo en el local storage
+//funcion de guardarlo en el localhost
 window.addEventListener('load', () => {
     const savedScore = localStorage.getItem('score');
     if (savedScore !== null) {
@@ -102,7 +63,7 @@ window.addEventListener('load', () => {
 });
 
 
-// meter el objeto del nave
+//funcion para que los objetos sean las naves
 function initScene() {
     let orbits = document.querySelectorAll('.orbit')
   
