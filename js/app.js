a5e4216cf3cb945063b6859d5620b47cfa1355d3
Kibria <kibriare@gmail.com>
2023-07-28 09:32:06 +0200

    subiendo página final

--- js/app.js
+++ js/app.js
@@ -1,9 +1,4 @@
-
-
-
-window.addEventListener('load', initScene, sound="src: #escena; autoplay")
-
-const meteors  = [
+const meteors = [
   { x: 0, y: 0, z: -60 },
   { x: 0, y: 0, z: 60 },
   { x: 20, y: 0, z: 0 },
@@ -12,31 +7,52 @@ const meteors  = [
   { x: 10, y: 0, z: -40 },
   { x: -10, y: 0, z: -40 },
   { x: -10, y: 0, z: 40 }
-]
+];
 
-let meteor, score = 0
+let score = 0;
 
-
-window.addEventListener('load', initScene)
-
-//funcion de disparar nave
+// Función de disparar nave
 AFRAME.registerComponent('shootable', {
-    init: function () {
-        this.el.addEventListener('click', () => {
-            // Increment the score
-            score++;
-            document.querySelector('[text]').setAttribute('value', `${score} naves eliminadas`);
+  init: function () {
+      this.el.addEventListener('click', () => {
+          // Increment the score
+          score++;
+          document.querySelector('[text]').setAttribute('value', `${score} naves eliminadas`);
 
-            // Remove the meteor from the scene
-            this.el.parentNode.removeChild(this.el);
+          // Check if 16 naves have been eliminated
+          if (score >= 16) {
+              // Redirect to the final.html page
+              window.location.href = 'final.html';
+          }
 
-            // Store the updated score in LocalStorage
-            localStorage.setItem('score', score);
-        });
-    },
+          // Remove the meteor from the scene
+          this.el.parentNode.removeChild(this.el);
+
+          // Store the updated score in LocalStorage
+          localStorage.setItem('score', score);
+      });
+  },
 });
 
-//funcion de guardarlo en el localhost
+
+// Función para que los objetos sean las naves
+function initScene() {
+    let orbits = document.querySelectorAll('.orbit');
+
+    orbits.forEach(orbit => {
+        meteors.forEach(pos => {
+            const meteor = document.createElement('a-entity');
+            meteor.setAttribute('gltf-model', 'https://cdn.glitch.global/ebd0de65-7a0c-4c3d-a550-d214da511d08/Turret%20Teleporter.glb?v=1690187026653');
+            meteor.setAttribute('material', { color: '#00FF00', antialias: 'true', sound: "src: #sound; on: click" });
+            meteor.setAttribute('class', 'meteor');
+            meteor.object3D.position.set(pos.x, pos.y, pos.z);
+            meteor.setAttribute('scale', '9 9 9');
+            meteor.setAttribute('shootable', '');
+            orbit.appendChild(meteor);
+        });
+    });
+}
+
 window.addEventListener('load', () => {
     const savedScore = localStorage.getItem('score');
     if (savedScore !== null) {
@@ -48,25 +64,3 @@ window.addEventListener('load', () => {
 });
 
 
-//funcion para que los objetos sean las naves
-function initScene() {
-    let orbits = document.querySelectorAll('.orbit')
-  
-    orbits.forEach(orbit => {
-  
-      meteors.forEach(pos => {
-  
-        meteor = document.createElement('a-entity')
-        meteor.setAttribute('gltf-model', 'https://cdn.glitch.global/ebd0de65-7a0c-4c3d-a550-d214da511d08/Turret%20Teleporter.glb?v=1690187026653') 
-        meteor.setAttribute('material', { color: '#00FF00', antialias: 'true', sound:"src: #sound; on: click" }) 
-        meteor.setAttribute('class', 'meteor')
-        meteor.object3D.position.set(pos.x, pos.y, pos.z)
-
-        meteor.setAttribute('scale', '9 9 9');
-  
-        meteor.setAttribute('shootable', '')
-  
-        orbit.appendChild(meteor)
-      })
-    })
-  }
