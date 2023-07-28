80acd98b598e860cf5da30e5ffc4953cdb6769ce
Greiskel <greiskel.sanchez@gmail.com>
2023-07-28 14:34:32 +0200

    actualizando escena

--- js/app.js
+++ js/app.js
@@ -11,31 +11,31 @@ const meteors = [
 
 let score = 0;
 
-// Función de disparar nave
+// Función de disparar 
 AFRAME.registerComponent('shootable', {
   init: function () {
       this.el.addEventListener('click', () => {
-          // Increment the score
+          // score
           score++;
           document.querySelector('[text]').setAttribute('value', `${score} naves eliminadas`);
 
-          // Check if 20 naves have been eliminated
+          
           if (score >= 20) {
-              // Redirect to the final.html page
+          
               window.location.href = 'final.html';
           }
 
-          // Remove the meteor from the scene
+          // Remove scene
           this.el.parentNode.removeChild(this.el);
 
-          // Store the updated score in LocalStorage
+          // localStorage
           localStorage.setItem('score', score);
       });
   },
 });
 
 
-// Función para que los objetos sean las naves
+// Función naves
 function initScene() {
     let orbits = document.querySelectorAll('.orbit');
 
@@ -62,94 +62,15 @@ window.addEventListener('load', () => {
 
     initScene();
 });
-// window.addEventListener('load', initScene, sound="src: #escena; autoplay")
-
-// const meteors  = [
-//   { x: 0, y: 0, z: -60 },
-//   { x: 0, y: 0, z: 60 },
-//   { x: 20, y: 0, z: 0 },
-//   { x: -20, y: 0, z: 0 },
-//   { x: 10, y: 0, z: 40 },
-//   { x: 10, y: 0, z: -40 },
-//   { x: -10, y: 0, z: -40 },
-//   { x: -10, y: 0, z: 40 }
-// ]
-
-// let meteor, score = 0
-
-
-// window.addEventListener('load', initScene)
-
-// //funcion de disparar nave
-// AFRAME.registerComponent('shootable', {
-//     init: function () {
-//         this.el.addEventListener('click', () => {
-//             // Increment the score
-//             score++;
-//             document.querySelector('[text]').setAttribute('value', `${score} naves eliminadas`);
-
-
-//           //   if (score >= 32) {
-//           //     // Redirect to the final.html page
-//           //     window.location.href = 'final.html';
-//           // } else {
-            
-//           // }
-
-//             // Remove the meteor from the scene
-//             this.el.parentNode.removeChild(this.el);
-
-//             // Store the updated score in LocalStorage
-//             localStorage.setItem('score', score);
-//         });
-//     },
-// });
-
-// //funcion de guardarlo en el localhost
-// window.addEventListener('load', () => {
-//     const savedScore = localStorage.getItem('score');
-//     if (savedScore !== null) {
-//         score = parseInt(savedScore);
-//         document.querySelector('[text]').setAttribute('value', `${score} naves eliminadas`);
-//     }
-
-//     initScene();
-// });
-
-
-// //funcion para que los objetos sean las naves
-// function initScene() {
-//     let orbits = document.querySelectorAll('.orbit')
-  
-//     orbits.forEach(orbit => {
-  
-//       meteors.forEach(pos => {
-  
-//         meteor = document.createElement('a-entity')
-//         meteor.setAttribute('gltf-model', 'https://cdn.glitch.global/ebd0de65-7a0c-4c3d-a550-d214da511d08/Turret%20Teleporter.glb?v=1690187026653') 
-//         meteor.setAttribute('material', { color: '#00FF00', antialias: 'true', sound:"src: #sound; on: click" }) 
-//         meteor.setAttribute('class', 'meteor')
-//         meteor.object3D.position.set(pos.x, pos.y, pos.z)
-
-//         meteor.setAttribute('scale', '9 9 9');
-  
-//         meteor.setAttribute('shootable', '')
-  
-//         orbit.appendChild(meteor)
-//       })
-//     })
-//   }
-
-  
 
 
 
+function showModal() {
+    var modal = document.getElementById("modal");
+    modal.style.display = "block";
+}
 
 
-
-
-
-
-
-
-  
+window.addEventListener('load', function () {
+    showModal();
+});
--- js/portal.js
+++ js/portal.js
@@ -32,45 +32,22 @@ AFRAME.registerComponent('generator', {
       sceneEl.appendChild(obj);
 
       obj.addEventListener('animationcomplete', function () {
-        // Cuando termina la animación, eliminamos el objeto actual
+        //animación
         sceneEl.removeChild(obj);
       });
     }
 
-    // Generar un objeto inmediatamente al inicio
     createAndAnimateObject();
 
-    // Generar objetos de uno en uno cada 8000ms (tiempo total de la animación anterior más el tiempo de espera)
     setInterval(function () {
       createAndAnimateObject();
     }, 1000);
   }
 });
  
-// const sound = document.getElementById('#soundnarracion');
-// // sonido = document.getElementById('a-scene')
-// sonido.setAttribute(sound,'"src: #soundnarracion; on: click"');
 
 window.addEventListener('DOMContentLoaded', () => {
   const soundnarracion = document.getElementById('#soundnarracion');
   soundnarracion.play();
 });
 
-// window.addEventListener('DOMContentLoaded', () => {
-//   const sound = document.getElementById('#soundnarracion');
-
-//   // Agregar un controlador de eventos "ended" al elemento de audio
-//   sound.addEventListener('ended', () => {
-//       // Detener el sonido y reiniciar el tiempo de reproducción al finalizar
-//       sound.pause();
-//       sound.currentTime = 0; // Reiniciar el tiempo de reproducción al principio
-//   });
-// });
-  
-// window.addEventListener('DOMContentLoaded', () => {
-//   const soundnarracion = document.getElementById('soundnarracion', click);
-//   soundnarracion.play();
-  
-// });
-  
-  
